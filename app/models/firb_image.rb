fca45ad8a1b06e75d36ece32c2e74ec3bf681a6b
Daniel Hahn <ghub@limitedcreativity.org>
2010-03-12 09:53:59 +0100

    Initial Firb Images Controller

---
+++ app/models/firb_image.rb
@@ -0,0 +1,36 @@
+class FirbImage < TaliaCore::Source
+  hobo_model # Don't put anything above this
+  
+  fields do
+    uri :string
+  end
+  
+  declare_attr_type :name, :string
+  
+  def uri
+    self[:uri]
+  end
+  
+  set_table_name "active_sources"
+  
+  def name=(name)
+    self.uri = (N::LOCAL + name).to_s
+  end
+  
+  def name
+    self.uri.nil? ? nil : self.uri.to_uri.local_name
+  end
+  
+  def create_permitted?
+    acting_user.administrator?
+  end
+  
+  def update_permitted?
+    acting_user.administrator?
+  end
+  
+  def view_permitted?(field)
+    true
+  end
+  
+end
