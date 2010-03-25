a719b7e514d240eb896f0c90bb5493dbb0a44944
simonefonda <fonda@netseven.it>
2010-03-25 16:51:59 +0100

    FirbTextPage intermediate commit: new action is working, now to the cards

--- app/controllers/admin/firb_text_pages_controller.rb
+++ app/controllers/admin/firb_text_pages_controller.rb
@@ -3,5 +3,17 @@ class Admin::FirbTextPagesController < Admin::AdminSiteController
   hobo_model_controller
 
   auto_actions :all
+
+  def create
+    txt = FirbTextPage.new(N::LOCAL + 'firbtextpage/' + FirbImageElement.random_id)
+    txt.parafrasi = params[:firb_text_page][:parafrasi]
+
+    if(txt.save)
+      flash[:notice] = "Text page succesfully created"
+    else
+      flash[:notice] = "Error creating the page"
+    end
+    redirect_to :controller => :firb_text_page, :action => :index
+  end
   
 end
--- app/models/firb_text_page.rb
+++ app/models/firb_text_page.rb
@@ -6,15 +6,17 @@ class FirbTextPage < TaliaCore::Source
   # These are single-value properties, the system will make sure
   # that there is only one value at a time
   singular_property :title, N::DCNS.title
+  singular_property :parafrasi, N::DCT.description
   
-  # Declare methods (getter/setter pairs) that should be used as
-  # fields by hobo. The type will be used by the automatic 
-  # forms to decide the input type.
   fields do
     uri :string
   end
 
+  # Declare methods (getter/setter pairs) that should be used as
+  # fields by hobo. The type will be used by the automatic 
+  # forms to decide the input type.
   declare_attr_type :title, :string
+  declare_attr_type :parafrasi, :text
   
   # Multi-value stuff:
   # - No direct support through hobo as a field 
