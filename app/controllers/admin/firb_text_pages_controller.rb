9ab84d814eb6677d0f145ec8d6be56c7cdd727d3
simonefonda <fonda@netseven.it>
2010-03-26 10:57:21 +0100

    Intermediate commit: working on new.dryml of FirbTextPage controller

--- app/controllers/admin/firb_text_pages_controller.rb
+++ app/controllers/admin/firb_text_pages_controller.rb
@@ -5,8 +5,7 @@ class Admin::FirbTextPagesController < Admin::AdminSiteController
   auto_actions :all
 
   def create
-    txt = FirbTextPage.new(N::LOCAL + 'firbtextpage/' + FirbImageElement.random_id)
-    txt.parafrasi = params[:firb_text_page][:parafrasi]
+    txt = create_page(params[:firb_text_page][:parafrasi])
 
     if(txt.save)
       flash[:notice] = "Text page succesfully created"
--- app/models/firb_text_page.rb
+++ app/models/firb_text_page.rb
@@ -7,6 +7,7 @@ class FirbTextPage < TaliaCore::Source
   # that there is only one value at a time
   singular_property :title, N::DCNS.title
   singular_property :parafrasi, N::DCT.description
+  singular_property :anastatica, N::DCT.isPartOf
   
   fields do
     uri :string
@@ -21,6 +22,29 @@ class FirbTextPage < TaliaCore::Source
   # Multi-value stuff:
   # - No direct support through hobo as a field 
   # - you can use the page.namespace:name notation
+
+  def self.create_page(parafrasi, anastatica)
+    p = FirbTextPage.new(N::LOCAL + 'firbtextpage/' + FirbImageElement.random_id)
+    txt.parafrasi = parafrasi
+    if (!anastatica.blank?)
+      self[N::DCT.isPartOf] << anastatica
+      zone
+    end
+    txt
+  end
+
+  def has_anastatica_page?
+    anastatica_page.blank?
+  end
   
+  def self.foo_anastatiche
+    FirbAnastaticaPage.all.collect{|a| ["#{a.title}: #{a.id}", a.id]}
+  end
+  
+  def anastatica_page
+    qry = ActiveRDF::Query.new(FirbAnastaticaPage).select(:p).distinct
+    qry.where(self, N::DCT.isPartOf, :p)
+    qry.execute.first
+  end
   
 end
