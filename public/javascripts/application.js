d1bb05cf82aba50e13aed88aa02998f14aca2d17
simonefonda <fonda@netseven.it>
2010-03-30 12:29:29 +0200

    Some little css tweaking and some translation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -38,12 +38,13 @@ function jsapi_mouseClick(fid, ki) {
 // uses the same markup as in views/admin/firb_text_pages/new.dryml
 // Used in that same dryml template
 function textPageAddNote() {
-    var markup = "<span class='firb-note'><a href='#' class='firb-remove-note'>Elimina nota</a><textarea rows='4' cols='50' name='firb_text_page[note][]'></textarea></span>";
+    var markup = "<div class='firb-note'><textarea rows='4' cols='50' name='firb_text_page[note][]'></textarea><span class='firb-remove-note'>Elimina nota</span></div>";
     $$('#firb-notes')[0].insert({bottom: markup});
 }
 
 document.observe('click', function(e) {
     if (e.element().match('.firb-remove-note')) {
         e.findElement('.firb-note').remove();
+        e.stop();
     }
 });
