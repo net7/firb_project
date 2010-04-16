b4d225850809880d0c48d9ae0964c3e0b9b8f404
simonefonda <fonda@netseven.it>
2010-04-16 12:31:22 +0200

    Working on ill. mem. dep. pages, css, cards etc

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -29,24 +29,24 @@ module AdminHelper
       %(<div id="#{status}">#{flash[status]}</div>) unless flash[status].nil?
     end
   end
-  
+
   # Defines the pages that are visible in the menu
   def active_pages
     %w(users background sources) # translations not working at the moment, templates not ready for generic use
   end
-  
+
   def original_image_url(image)
     url_for_data_record(image.original_image)
   end
-  
+
   def thumb_url(image)
     url_for_data_record(image.iip_record)
   end
-  
+
   def thumb_xml(image, zone_list=nil)
     image.zones_xml(thumb_url(image), zone_list)
   end
-  
+
   def image_xml(image)
     image.zones_xml(original_image_url(image))
   end
@@ -58,6 +58,23 @@ module AdminHelper
       ''
     end
   end
-  
-end
 
+  # Works on sources which have an associated image_zone (text_page, illustrated memory
+  # depictions, ..). Will return its parent image_zone along with its name and the
+  # associated image_zone and it's name, if there's any. 
+  def parent_and_zone(object)
+    if !object.image_zone.nil?
+    		parent = object.image_zone.get_firb_image_parent
+    		parent_name = parent.name
+    		image_zone = object.image_zone
+    		image_zone_name = image_zone.name
+		else
+		    parent = nil
+    		image_zone = nil
+    		parent_name = I18n.t("firb.no_associated_image")
+    		image_zone_name = I18n.t("firb.no_associated_zone")
+		end
+		return parent, parent_name, image_zone, image_zone_name
+  end
+
+end
