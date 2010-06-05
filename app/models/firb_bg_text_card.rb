53c8d829d9f05b6c24639b78d24c56c17869918f
simonefonda <fonda@netseven.it>
2010-06-05 12:29:24 +0200

    Firb BG text_cards up and running

---
+++ app/models/firb_bg_text_card.rb
@@ -0,0 +1,30 @@
+class FirbBgTextCard < FirbTextCard
+
+  hobo_model
+  include StandardPermissions
+
+  rdf_property :title, N::DCNS.title
+
+  # Link all'anastatica
+  singular_property :anastatica, N::DCT.isPartOf, :force_relation => true
+
+  # Numero carta
+  rdf_property :page_position, N::TALIA.position
+
+  # Collocazione
+  rdf_property :collocation, N::TALIA.provenance
+
+  # Segnatura
+  rdf_property :signature, N::TALIA.signature
+
+  # Autore
+  rdf_property :author, N::DCT.creator
+
+  # Commento
+  rdf_property :comment, N::TALIA.comment, :type => 'text'
+
+  fields do
+    uri :string
+  end
+  
+end
