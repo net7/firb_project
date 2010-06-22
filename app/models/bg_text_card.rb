9b89ac912541135272309431b88c194c020ef781
simonefonda <fonda@netseven.it>
2010-06-22 12:15:07 +0200

    Bg text card book link {T: 40}

--- app/models/bg_text_card.rb
+++ app/models/bg_text_card.rb
@@ -23,8 +23,45 @@ class BgTextCard < TextCard
   # Commento
   rdf_property :comment, N::TALIA.comment, :type => 'text'
 
+  # Libro a cui appartiene
+  manual_property :book
+  before_validation :validate_book
+  after_save :save_book
+
   fields do
     uri :string
   end
+
+  def book
+    @book ||= fetch_book
+  end
   
+  def book=(value)
+    @book = (value.is_a?(TaliaCollection) ? value : TaliaCollection.find(value)).real_source
+    @book_new = true
+    @book << self
+  end
+  
+  def validate_book
+    if(!book_valid?)
+      @book.errors.each_full { |msg| errors.add('book', msg) }
+    end
+    book_valid?
+  end
+  
+  def save_book
+    is_new = @book_new
+    @book_new = false
+    is_new ? @book.save : book_valid?
+  end
+  
+  def fetch_book
+    book = TaliaCollection.find(:first, :find_through => [N::DCT.hasPart, self.uri])    
+    book ? book.real_source : nil
+  end
+  
+  def book_valid?
+    @book ? @book.valid? : true
+  end
+    
 end
