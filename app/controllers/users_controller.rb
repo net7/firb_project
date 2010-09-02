1c1d6a8c3090d72662e247672fb5d05a964c7f46
Danilo Giacomi <giacomi@netseven.it>
2010-09-02 18:58:04 +0200

    added remote (from boxView UI) signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,57 @@ class UsersController < ApplicationController
 
   hobo_user_controller
 
-
   auto_actions :all, :except => [ :index, :new, :create ]
 
   def base_url
     (ActionController::Base.relative_url_root || "") + '/admin/'
   end
 
-  def do_signup
-    do_creator_action :signup, :redirect => TaliaCore::CONFIG['login_redirect_url'] 
+
+  
+  def json_signup
+    error = 0
+    data = ''
+    html = ''
+    @user = User.find_by_email_address(params[:user][:email_address])
+    if @user
+      # return the user already exists json
+      html = 'Utente o Password esistenti'
+      # no we can't have a common render at the bottom of file,
+      # the things inside do_creator_action would try to render something else
+      render :json => {'error' => error,
+        'html' => html,
+        'data' => data
+      } and return
+    else
+      do_creator_action :signup do
+        if valid?
+          # success, return some json
+          html =  'Utente creato con successo'
+          # we must render it now or hobo/rails will render something on their own
+          render :json => {'error' => error,
+            'html' => html,
+            'data' => data
+          } and return
+        else
+          # we have some errors
+          html = '<ul>'
+          this.errors.full_messages.each do |error|
+            html = "#{html} <li> #{error.to_s} </li>"
+          end
+          html = "#{html} </ul>"
+          error = 1
+          # we must render it now or hobo/rails will render something on their own
+          render :json => {'error' => error,
+            'html' => html,
+            'data' => data
+          } and return
+
+        end
+      end
+    end
   end
+
 
   
 end
