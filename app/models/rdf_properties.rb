7d01b9185bcdfda482e1b8c1d5acd1d5bf89d93a
Danilo Giacomi <giacomi@netseven.it>
2010-10-05 16:36:29 +0200

    rdf_property used to "eat" the :type parameter instead of passing it through to singular_property. Now it does so, provided it is a class

--- app/models/rdf_properties.rb
+++ app/models/rdf_properties.rb
@@ -2,7 +2,11 @@ module RdfProperties
   
   def rdf_property(shortcut, property, options = {})
     options.to_options!
-    type = (options.delete(:type) || :string)
+#    type = (options.delete(:type) || :string)
+    type = (options[:type] || :string)
+    unless type.is_a?(Class)
+      options.delete(:type)
+    end
     singular_property shortcut, property, options
     declare_attr_type shortcut, type
   end
