e68e79a892d7ab453002cd79c188b793b401431c
simonefonda <fonda@netseven.it>
2010-11-09 17:42:09 +0100

    custom_bibl_item model in place; insertion partial up n running, testing first on PiIllustrationCards

---
+++ app/models/custom_bibliography_item.rb
@@ -0,0 +1,18 @@
+class CustomBibliographyItem < TaliaCore::Source
+  hobo_model # Don't put anything above this
+  include StandardPermissions
+  extend RdfProperties
+  
+  autofill_uri :force => true
+  
+  rdf_property :name, N::TALIA.name
+  rdf_property :pages, N::DCT.pages
+
+  # Local bibliography item, the kind we already have
+  rdf_property :bibliography_item, N::TALIA.bibliographyItem, :type => TaliaCore::ActiveSource
+
+  # remote bibliography item, probably a URI for some bibl-store somewhere on the interwebs
+  # TODO: change type :string to .. ActiveSource? Another active type? Something new?
+  rdf_property :remote_bibliography_item, N::TALIA.remoteBibliographyItem, :type => :string
+  
+end
