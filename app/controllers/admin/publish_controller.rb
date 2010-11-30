ac7ca6469bc7462fa165b7645e9cee7a12dcf8b9
simonefonda <fonda@netseven.it>
2010-11-30 18:56:39 +0100

    Publish methods in place up and running. PiTextCards are the first to implement it, with no special actions on publish.. just setting the flag for now. UI element added direclty in the cards to publish/unpublish items.

--- app/controllers/admin/publish_controller.rb
+++ app/controllers/admin/publish_controller.rb
@@ -1,14 +1,9 @@
 class Admin::PublishController < Admin::AdminSiteController
-
-  def index
-    render :text => "ACF"
-  end
   
   def toggle
     @source = TaliaCore::ActiveSource.find(params[:id])
-    @username = current_user.name + " ("+current_user.email_address.gsub("@","_AT_")+")"
-    @source.toggle_published_by(@username)
-    render :text => "ACF: #{@source.name}"
-  end
+    @source.toggle_published_by(current_user.name)
+    hobo_ajax_response if request.xhr?
+   end
   
 end
