bccd87ce833cc4fa6bb6b9bf83c8d7413595147f
simonefonda <fonda@netseven.it>
2010-12-02 15:18:31 +0100

    Using logger instead of puts for tommy tomcat

--- app/controllers/admin/publish_controller.rb
+++ app/controllers/admin/publish_controller.rb
@@ -7,14 +7,14 @@ class Admin::PublishController < Admin::AdminSiteController
       jaxer_url = TaliaCore::CONFIG['jaxer_url']
       this_page_url = url_for(:controller => "/admin/base_cards", :action => :show_annotable, :id => @source.data_records.find_by_type_and_location('TaliaCore::DataTypes::XmlData','html1.html').id)
 
-      puts "@@@@@@@@@@@@@@@@@@ " + " Jaxer: "+jaxer_url.to_s + " >> " + this_page_url
+      logger.warn "@@@@@@@@@@@@@@@@@@ " + " Jaxer: "+jaxer_url.to_s + " >> " + this_page_url
 
       #args = {'html1' => render_to_string something somewhere someway somewhat }
       #resp, data = Net::HTTP.post_form(jaxer_url, args)
       puts URI.parse(jaxer_url + "?uri=" + this_page_url).to_s
 
       resp = Net::HTTP.get(URI.parse(jaxer_url + "?uri=" + this_page_url))
-      puts "@@@@@@@@@@@@@@@@@@ " + this_page_url + "\n\n >> " + resp.to_s
+      logger.warn "@@@@@@@@@@@@@@@@@@ " + this_page_url + "\n\n >> " + resp.to_s
     end
     @source.toggle_published_by(current_user.name)
     hobo_ajax_response if request.xhr?
