b984a9134a77a5dc73ec7e72368cab9179d35d01
Simone Fonda <fonda@netseven.it>
2011-04-07 11:08:24 +0200

    Paginating dictionary items by type

--- app/controllers/admin/dictionary_items_controller.rb
+++ app/controllers/admin/dictionary_items_controller.rb
@@ -2,5 +2,15 @@ class Admin::DictionaryItemsController < Admin::AdminSiteController
 
   hobo_model_controller
   auto_actions :all
-  
+
+def index
+
+    conditions = { :prefetch_relations => true, :include => :data_records }
+    if (params[:type])
+      conditions.merge!(:find_through => [N::TALIA.dictionary_item_type, params[:type]])
+    end
+
+    @dictionary_items = DictionaryItem.paginate(conditions.merge(:page => params[:page]))
+  end
+
 end
