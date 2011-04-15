f5a7277e804a4916223748a531ce2b417a898f1d
Simone Fonda <fonda@netseven.it>
2011-04-15 16:58:53 +0200

    Adding "animali" to pi component types

--- app/helpers/admin/text_card_helper.rb
+++ app/helpers/admin/text_card_helper.rb
@@ -111,7 +111,7 @@ module Admin::TextCardHelper
   # Placeholder for something more appropriate: something which gathers the real 
   # types from an ontology or some db thingie
   def pi_component_types
-    ["luoghi", "numeri", "lettere dell'alfabeto", "persone", "oggetti"].collect{ |t| [t, t] }
+    ["animali", "luoghi", "numeri", "lettere dell'alfabeto", "persone", "oggetti"].collect{ |t| [t, t] }
   end
 
   # Placeholder for something more appropriate: something which gathers the real 
