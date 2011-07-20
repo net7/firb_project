d528024319df67b9275e121a4655538a6d93c72e
Simone Fonda <fonda@netseven.it>
2011-07-20 14:23:26 +0200

    Implementing annotated content coming from consolidator: - notes gets displayed correctly (need some more test) - instanceOf gets handled, need to add some classes to the annotated content and link those classes to the fenomeni window (in construction) - images gets replaced with an IMT version, if an image zone has been linked; still need to pass the correct zones to the image (zones of the same image coming from annotations); link the text to the image zones and vice-versa; - still need to handle the hasImageZone relations, modifying the markup to handle (inside fenomeni window? inside notes? .. whatever)
    Minor css improvements to support changes and javascripts, Giulio will need to see them and add some rules.

--- app/controllers/boxview/pi_scheda_testo_controller.rb
+++ app/controllers/boxview/pi_scheda_testo_controller.rb
@@ -10,29 +10,85 @@ class Boxview::PiSchedaTestoController < Boxview::BaseController
     
     @content = ''
     @notes = []
+    @fenomeni = []
+
     v = Nokogiri::HTML.parse(@raw_content)
+    
+    # Replace a tagged <img> with an IMT container initalized with the 
+    # given zones
+    v.xpath(".//img[@class='source_img'][contains(@about, '/zones/')]").each do |d|
+      @z = ImageZone.find(d['about'], :prefetch_relations => true)
+      @image = @z.get_image_parent
+      
+      imt = "<div><a title='Mostra immagine' class='transcription_image_icon'>SHOW IMAGE</a>"
+      imt += "<span class='transcription_img_wrapper hidden'>"
+      imt += render_to_string :partial => '/boxview/shared/imageviewer', 
+               :locals => {:id => rand(Time.now.to_i), 
+                           :base64 => @image.zones_xml(@image.uri, [@z.uri.to_s]),
+                           :js_prefix => 'jsapi'}
+      imt += '<a title="Apri in un bel box" class="transcription_open_icon">APRI IN BOX</a>'
+      imt += '<a title="Chiudi" class="transcription_close_icon">CHIUDI</a>'
+      imt += "</span></div>"
+
+      d.replace(Nokogiri::HTML.parse(imt).xpath(".//div")[0])
+      
+      # Find and remove the consolidated annotation with this image zone
+      v.xpath(".//div[@class='consolidatedAnnotation'][contains(@about, '#{d['about']}')]").each do |ca|
+        sub_lab = ca.xpath(".//div[@class='subject']/span[@class='label']")[0].text
+        if (sub_lab.include? '[image: illustration.jpg]')
+          ca.remove
+        end
+      end
+      
+    end
+    
+    
     v.xpath(".//div[@class='consolidatedAnnotation']").each do |d|
       pred = d.xpath(".//div[@class='predicate']")[0]['about'];
+      ca_class = d.xpath(".//span[@class='annotationClass']")[0].text
+
       if (pred == 'http://purl.oclc.org/firb/swn_ontology#hasNote')
-        n_class = d.xpath(".//span[@class='annotationClass']")[0].text
         n_name = d.xpath(".//div[@class='object']/span[@class='name']")[0].text
         n_content = d.xpath(".//div[@class='object']/span[@class='content']")[0].text
-        @notes.push({:name => n_name, :content => n_content, :class => n_class})
+        @notes.push({:name => n_name, :content => n_content, :class => ca_class})
+        d.remove
+      end
+      
+      if (pred == 'http://purl.oclc.org/firb/swn_ontology#instanceOf')
+        di_id = d.xpath(".//div[@class='object']")[0]['about']
+        di = DictionaryItem.find(di_id, :prefetch_relations => true)
+        di_type = di.item_type.slice(41,100)
+        @fenomeni.push({:name => di.name, :item_type => di_type, :class => ca_class })
+        d.remove
+      end
+      
+      if (pred == "http://purl.oclc.org/firb/swn_ontology#hasImageZone")
+        
+      end
+      
+      if (pred == 'http://purl.oclc.org/firb/swn_ontology#hasMemoryDepiction')
+        md_id = d.xpath(".//div[@class='object']")[0]['about']
+
+        begin
+          md = PiIllustratedMdCard.find(md_id, :prefetch_relations => true)
+        rescue
+          md = PiNonIllustratedMdCard.find(md_id, :prefetch_relations => true)
+        else
+          @fenomeni.push({:name => md.short_description, :item_type => "Immagini di memoria"})
+          d.remove
+        end
+          
       end
       #d.remove
     end
 
-    # Replace a tagged <img> tag with an IMT container initalized with the 
-    # given zone
-    v.xpath(".//img[@class='source_img'][contains(@about, '/zones/')]").each do |d|
-      @z = ImageZone.find(d['about'], :prefetch_relations => true)
-      @image = @z.get_image_parent
-      imt = render_to_string :partial => '/boxview/shared/imageviewer', 
-               :locals => {:id => rand(Time.now.to_i), 
-                           :base64 => @image.zones_xml(@image.uri, [@z.uri.to_s]),
-                           :js_prefix => 'jsapi'}
-      d.replace(Nokogiri::HTML.parse(imt).xpath(".//div"))
-    end
+    @fenomeni.sort! { |a,b| 
+      if (a[:item_type].downcase == b[:item_type].downcase)
+        a[:name].downcase <=> b[:name].downcase 
+      else
+        a[:item_type].downcase <=> b[:item_type].downcase 
+      end
+    }
     
     @content << v.to_s
     
