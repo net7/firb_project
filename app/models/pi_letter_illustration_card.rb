b63f51d55ec834d21a414d46e094070a7f7a84d6
Simone Fonda <fonda@netseven.it>
2011-07-28 15:58:53 +0200

    Adding link_id to boxview_link helper (and to boxview_link_for_object as well); added "elementi correlati" widget to anastatica box, should get clicked on IMT zones click [touch:205]

--- app/models/pi_letter_illustration_card.rb
+++ app/models/pi_letter_illustration_card.rb
@@ -10,4 +10,16 @@ class PiLetterIllustrationCard < IllustrationCard
   # Missing: 
   # list of decorative components, with: name, type (floreal, human, ..)
   # illustration's components
+
+  def boxview_data
+    desc = self.description.nil? ? "" : "#{self.description.slice(0, 80)}.."
+    { :controller => 'boxview/illustrazioni_lettera', 
+      :title => "Capolettera #{desc}", 
+      :description => desc,
+      :res_id => "pi_letter_illustration_#{self.id}", 
+      :box_type => 'image',
+      :thumb => nil
+    }
+  end
+
 end
