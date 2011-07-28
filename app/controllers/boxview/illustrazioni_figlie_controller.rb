3d8368589c9a895fdfe565390c55a996c10a38bf
Danilo Giacomi <giacomi@netseven.it>
2011-07-28 16:36:07 +0200

    first try

--- app/controllers/boxview/illustrazioni_figlie_controller.rb
+++ app/controllers/boxview/illustrazioni_figlie_controller.rb
@@ -1,5 +1,7 @@
 class Boxview::IllustrazioniFiglieController < Boxview::BaseController
   def show
     @resource = PiIllustratedMdCard.find_by_id params[:id]
+    @zone     = @resource.image_zone
+    @image    = @zone.get_image_parent
   end
 end
