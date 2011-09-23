0da3282537582bb7b140aaf4feb54d451cd753c4
rik <giomi@netseven.it>
2011-09-23 11:49:40 +0200

    [touch: 293]{t: 16} more SOLR fields for printed cards

--- app/models/solr/vt_printed_text_card.rb
+++ app/models/solr/vt_printed_text_card.rb
@@ -1,34 +1,18 @@
 module SOLR
   class VtPrintedTextCard < Base
     solr_setup do
-      # text :signature
-      # text :watermark
-      # text :technical_notes
-      # text :conservation_status
-      # text :bibliography
-
-      # string :bibliography, :multiple => true
+      text :ref_edition
+      text :name
+      # text :page_position
 
       dynamic_string :facets, :multiple => true, :stored => true do
         facets
       end
-
-      # Other fields (filters?)
-      # measure
-      # cartulation
-      # autography ("Completa" for all cards?)
-      # date
     end
 
-    # def bibliography
-    #   bibliography_items.to_a.map do |item|
-    #     [].tap do |parts|
-    #       parts << item.bibliography_item.published_in.to_s
-    #       parts << item.bibliography_item.publisher.to_s
-    #       parts << item.bibliography_item.date.to_s
-    #       parts << item.pages.to_s
-    #     end.compact.join ', '
-    #   end
-    # end
+    def edition_ref
+      VtLetter.edition_title_for bibliography_items.first
+    end
+
   end # class VtPrintedTextCard
 end # module SOLR
