cbc8527560ee78f07b00979e6a5d2efc17496cbc
Danilo Giacomi <giacomi@netseven.it>
2011-10-06 00:19:19 +0200

    welcome back fonti

--- app/models/solr/fi_text_card.rb
+++ app/models/solr/fi_text_card.rb
@@ -8,10 +8,14 @@ module SOLR
       text :transcription_text
       text :facets
 
-      string :bibliography, :multiple => true, :stored => true
+      string :bibliography, :multiple => true, :stored => true do
+        bibliography_full_text
+      end
       dynamic_string :facets, :multiple => true, :stored => true do
         facets
       end
+
+
     end # solr_setup do
 
   end # class FiTextCard
